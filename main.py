3ec713a5302bc2d5006f24309821bcf5651ee06a
gruzewson <gruzewskimarcel@gmail.com>
2024-04-28 15:51:54 +0200

    removed using numpy

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-import numpy as np
+from cmath import sin
 
-# Parametry
-a1 = 5 + 1  # e = 1
+index = [1,9,3,5,8,9]
+c = index[4]
+d = index[5]
+e = index[3]
+f = index[2]
+N = 9 * 100 + c * 10 + d
+a1 = 5 + e
 a2 = a3 = -1
-c = 1
-d = 1
-f = 1
-N = 9 * c * d
 
-# Macierz A
-A = np.zeros((N, N))
+A = [[0]*N for _ in range(N)]
 for i in range(N):
     for j in range(N):
         if i == j:
-            A[i, j] = a1
+            A[i][j] = a1
         elif abs(i - j) == 1:
-            A[i, j] = a2
+            A[i][j] = a2
         elif abs(i - j) == 2:
-            A[i, j] = a3
+            A[i][j] = a3
 
-# Wektor b
-b = np.array([np.sin((n + 1) * (f + 1)) for n in range(N)])
+b = [0] * N
+for i in range(N):
+    b[i] = sin(i * (f+1))
 
 print("Macierz A:")
 print(A)
